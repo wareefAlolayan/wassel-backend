f574d01bcac0ad41029e56d5cbe54440df739701
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-26 20:36:53 +0300

    add serializers for team and employee

---
+++ main_app/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Team ,  Employee
+
+
+# this will take our django db object and make it into json 
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+            
+class EmployeeSerializer(serializers.ModelSerializer):
+    
+    team = TeamSerializer(many=False, read_only=True)
+    
+    class Meta:
+        model = Employee
+        fields = '__all__'
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .models import Team , Employee
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from .serializers import *
+from rest_framework.permissions import IsAuthenticated , AllowAny , IsAuthenticatedOrReadOnly
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your views here.
