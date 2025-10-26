2ddb69f5c38cbdf1656d3e6b3018115665d2ef91
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-26 23:14:08 +0300

    add TeamDetail view with patch request

--- main_app/views.py
+++ main_app/views.py
@@ -12,7 +12,6 @@ User = get_user_model()
 
 # Create your views here.
 class TeamsIndex(APIView):
-    # permission_classes = [IsAuthenticated]
     def get(self, request): # Read teams list
         try:
             queryset = Team.objects.all() #get all objects of model team
@@ -22,13 +21,24 @@ class TeamsIndex(APIView):
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def post(self, request): #Create new team
-        # permission_classes = [IsAuthenticated]
         try:
             serializer = TeamSerializer(data=request.data) #all data in request that is in json will be taken and converted to object
-            if serializer.is_valid(): #
-                serializer.save() #
+            if serializer.is_valid(): 
+                serializer.save() 
                 return Response(serializer.data ,status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-       
+
+class TeamDetail(APIView):
+    def patch(self, request, team_id): #update team details 
+        try:
+            queryset = get_object_or_404(Team, id=team_id) #find the team 
+            serializer = TeamSerializer(queryset, data=request.data) #pass to serializer 
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data , status=status.HTTP_200_OK) #updated
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
