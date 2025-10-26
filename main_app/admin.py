8df4440e4112ed6e783b912f315cd061e3fd1d73
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-26 19:45:12 +0300

    added Team and Employee models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Team , Employee
 # Register your models here.
+
+admin.site.register(Team)
+admin.site.register(Employee)
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+    
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    is_manager = models.BooleanField(default=False)
+    vaction_days_left = models.PositiveIntegerField()
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return f'{self.name} - {'Manager' if self.is_manager else 'Employee'}'
+  
