c0422474c078631d2abb964f57c841ee67cd1112
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-28 13:19:04 +0300

    add url to shiftIndex

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TeamsIndex , TeamDetail , EmployeesIndex
+from .views import TeamsIndex , TeamDetail , EmployeesIndex , ShiftIndex
 
 urlpatterns = [
     path('teams/', TeamsIndex.as_view(), name='team_index'),
     path('teams/<int:team_id>/' , TeamDetail.as_view() , name='team_detail'),
     path('employees/' , EmployeesIndex.as_view() , name='employees' ),
+    path('shifts/' , ShiftIndex.as_view() , name='shifts' ),
 ]
