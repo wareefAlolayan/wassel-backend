9604a0e9a9214900dc9b41dfdf069f5481be14c4
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-28 09:38:32 +0300

    add get method to TeamDetail

--- main_app/views.py
+++ main_app/views.py
@@ -31,6 +31,14 @@ class TeamsIndex(APIView):
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class TeamDetail(APIView):
+    def get(self,request , team_id):
+        try:
+            queryset =get_object_or_404(Team,id=team_id) 
+            serializer = TeamSerializer(queryset)
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+       
     def patch(self, request, team_id): #update team details 
         try:
             queryset = get_object_or_404(Team, id=team_id) #find the team 
