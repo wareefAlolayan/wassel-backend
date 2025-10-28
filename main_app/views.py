a96afc45ead9fc5575f8a2a26d23f24730536381
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-28 09:58:27 +0300

    added EmplyeesIndex view with get method

--- main_app/views.py
+++ main_app/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .models import Team , Employee
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from .serializers import TeamSerializer
+from .serializers import TeamSerializer , EmployeeSerializer
 from rest_framework.permissions import IsAuthenticated , AllowAny , IsAuthenticatedOrReadOnly
 from django.contrib.auth import get_user_model
 
@@ -58,3 +58,12 @@ class TeamDetail(APIView):
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
+class EmployeesIndex(APIView) :
+    def get(self, request): # Read Employee list
+        try:
+            queryset = Employee.objects.all() #get all objects of model Employee
+            serializer = EmployeeSerializer(queryset, many=True) #send it to serializer to convert object to json 
+            return Response(serializer.data ,  status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
