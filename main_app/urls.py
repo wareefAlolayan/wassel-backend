a84d231f468212a7be554335a4ffbadcedb40843
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-28 10:01:03 +0300

    add url to emplyeeIndex view

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TeamsIndex , TeamDetail
+from .views import TeamsIndex , TeamDetail , EmployeesIndex
 
 urlpatterns = [
     path('teams/', TeamsIndex.as_view(), name='team_index'),
-    path('teams/<int:team_id>/' , TeamDetail.as_view() , name='team_detail')
+    path('teams/<int:team_id>/' , TeamDetail.as_view() , name='team_detail'),
+    path('employees/' , EmployeesIndex.as_view() , name='employees' ),
 ]
