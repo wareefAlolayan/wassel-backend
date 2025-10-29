d60aefee4683431deeda35c8026fe4d3bc2dfe83
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-29 13:16:33 +0300

    added EmployeeDetail view with url

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TeamsIndex , TeamDetail , EmployeesIndex , ShiftIndex , ShiftDetail , AssignEmployee , UnassignEmployee
+from .views import TeamsIndex , TeamDetail , EmployeesIndex , ShiftIndex , ShiftDetail , AssignEmployee , UnassignEmployee , EmployeeDetail
 
 urlpatterns = [
     path('teams/', TeamsIndex.as_view(), name='team_index'),
     path('teams/<int:team_id>/' , TeamDetail.as_view() , name='team_detail'),
     path('employees/' , EmployeesIndex.as_view() , name='employees' ),
+    path('employees/<int:emp_id>/' , EmployeeDetail.as_view() , name='employee' ),    
     path('shifts/' , ShiftIndex.as_view() , name='shifts' ),
     path('shifts/<int:shift_id>' , ShiftDetail.as_view() , name='shift_detail' ),
     path('shifts/<int:shift_id>/assign/<int:employee_id>' , AssignEmployee.as_view() , name='assign_emp' ),
--- main_app/views.py
+++ main_app/views.py
@@ -66,7 +66,15 @@ class EmployeesIndex(APIView) :
             return Response(serializer.data ,  status=status.HTTP_200_OK)
         except Exception as error:
             return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+class EmployeeDetail(APIView):
+    def get(self,request , emp_id):
+        try:
+            queryset =get_object_or_404(Employee,id=emp_id) 
+            serializer = EmployeeSerializer(queryset)
+            return Response(serializer.data , status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+       
 class ShiftIndex(APIView) :
     def get (self,request) :
         try :
