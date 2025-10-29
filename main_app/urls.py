52b884a0bbdad06c4493fdc50e291d154dd1f9d5
wareefalolayan <wareefalolayan1424@gmail.com>
2025-10-29 10:52:48 +0300

    add urls to assign and unassign employees

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TeamsIndex , TeamDetail , EmployeesIndex , ShiftIndex , ShiftDetail
+from .views import TeamsIndex , TeamDetail , EmployeesIndex , ShiftIndex , ShiftDetail , AssignEmployee , UnassignEmployee
 
 urlpatterns = [
     path('teams/', TeamsIndex.as_view(), name='team_index'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('employees/' , EmployeesIndex.as_view() , name='employees' ),
     path('shifts/' , ShiftIndex.as_view() , name='shifts' ),
     path('shifts/<int:shift_id>' , ShiftDetail.as_view() , name='shift_detail' ),
+    path('shifts/<int:shift_id>/assign/<int:employee_id>' , AssignEmployee.as_view() , name='assign_emp' ),
+    path('shifts/<int:shift_id>/unassign/<int:employee_id>' , UnassignEmployee.as_view() , name='unassign_employee' ),
 ]
